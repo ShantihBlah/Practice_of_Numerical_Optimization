6c8d89b3aa21d32f736e93813744109b02b1f76f
ShantihBlah <1602822815@qq.com>
2022-07-01 01:45:31 +0800

    Add standard CG-Preliminary

--- Chapter_5/conjugate_gradient_preliminary.jl
+++ Chapter_5/conjugate_gradient_preliminary.jl
@@ -1,6 +1,6 @@
 using LinearAlgebra
 
-function conjugate_gradient(A_matrix, x_vec, b_vec)
+function conjugate_gradient_preliminary(A_matrix, x_vec, b_vec)
     tolerance = 1e-16
     x_vec_k = x_vec
     residual_k = A_matrix * x_vec_k - b_vec
@@ -8,10 +8,11 @@ function conjugate_gradient(A_matrix, x_vec, b_vec)
     k = 0
     while norm(residual_k) > tolerance
         # println(k, ": ", norm(residual_k))
-        alpha_k = (Transpose(residual_k)*residual_k) / (Transpose(p_k)*A_matrix*p_k)
+        quadratic_term = (Transpose(p_k)*A_matrix*p_k)
+        alpha_k = - (Transpose(residual_k)*p_k) / quadratic_term
         x_vec_k_1 = x_vec_k + alpha_k * p_k
-        residual_k_1 = residual_k + alpha_k * A_matrix * p_k
-        beta_k_1 = (Transpose(residual_k_1)*residual_k_1) / (Transpose(residual_k)*residual_k) 
+        residual_k_1 = A_matrix * x_vec_k_1 - b_vec
+        beta_k_1 = (Transpose(residual_k_1)*A_matrix*p_k) / quadratic_term
         p_k_1 = -residual_k_1 + beta_k_1*p_k
         x_vec_k = x_vec_k_1
         p_k = p_k_1
@@ -24,4 +25,3 @@ function conjugate_gradient(A_matrix, x_vec, b_vec)
     println(k)
     return x_vec_k
 end
-
--- solver_interface.jl
+++ solver_interface.jl
@@ -5,33 +5,36 @@ include("problems/unconstraint/Rosenbrock_func.jl")
 
 include("Chapter_3/line_search.jl")
 include("Chapter_4/trust_region.jl")
+include("Chapter_5/conjugate_gradient_preliminary.jl")
 include("Chapter_5/conjugate_gradient.jl")
 
-problem_setup = quadratic_setup
-# problem_setup = rosenbrock_setup
+# problem_setup = quadratic_setup
+# # problem_setup = rosenbrock_setup
 
-object_function = problem_setup["obj_func"]
-init_vec = problem_setup["init_vec"]
-init_y = object_function(init_vec)
-max_iter_num = 2000
+# object_function = problem_setup["obj_func"]
+# init_vec = problem_setup["init_vec"]
+# init_y = object_function(init_vec)
+# max_iter_num = 2000
 
-@time solution_vec = lineSearch(object_function, init_vec, max_iter_num, BK_method="Speedest_Descend")
-# @time solution_vec = trustRegion(object_function, init_vec, max_iter_num, delta_max=20)
+# @time solution_vec = lineSearch(object_function, init_vec, max_iter_num, BK_method="Speedest_Descend")
+# # @time solution_vec = trustRegion(object_function, init_vec, max_iter_num, delta_max=20)
 
-solution_y = object_function(solution_vec)
+# solution_y = object_function(solution_vec)
 
-println("Initial X: ", init_vec)
-println("Initial Y: ", init_y)
-println("Final X: ", solution_vec)
-println("Final Y: ", solution_y)
+# println("Initial X: ", init_vec)
+# println("Initial Y: ", init_y)
+# println("Final X: ", solution_vec)
+# println("Final Y: ", solution_y)
 
 
 rng = MersenneTwister(1234)
 
-dim = 50
+dim = 100
 B = randexp(rng, dim, dim)
 x = 5.0*ones(dim)
 b = zeros(dim)
 A = (Transpose(B) * B)
 
-@time x = conjugate_gradient(A, x, b)
+
+@time solution_x = conjugate_gradient_preliminary(A, x, b)
+@time solution_x = conjugate_gradient(A, x, b)
